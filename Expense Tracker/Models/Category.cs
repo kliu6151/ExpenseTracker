10764f4698624cbcaeb188fea7a5a996b285e002
kliu6151 <kevinliu3931@gmail.com>
2023-12-21 14:50:23 -0500

    front end

---
+++ Expense Tracker/Models/Category.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Category
+    {
+        [Key]
+        public int CategoryId { get; set; }
+
+        [Column(TypeName = "nvarchar(50)")]
+        public String Title { get; set; } = "";
+        [Column(TypeName = "nvarchar(5)")]
+        public String Icon { get; set; } = "";
+        [Column(TypeName = "nvarchar(10)")]
+
+        public String Type { get; set; } = "Expense";
+
+    }
+}
---
+++ Expense Tracker/Models/Transaction.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Expense_Tracker.Models
+{
+    public class Transaction
+    {
+        [Key]
+        public int TransactionId { get; set; }
+
+        //CategoryId, FOREIGN KEY
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        [Column]
+        public int Amount { get; set; }
+
+        [Column(TypeName = "nvarchar(75)")]
+        public String? Note { get; set; }
+        [Column]
+        public DateTime Date { get; set; } = DateTime.Now;
+     }
+}
